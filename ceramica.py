b38947bfb198cbd507ec1511248f22f28670b524
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-09-30 10:46:08 -0300

    Alterando "Ingredientes.imprime_ingredientes()", para usar a chave "{"id"}" de "ingredientes.json".

--- ceramica.py
+++ ceramica.py
@@ -5,27 +5,32 @@ class Receita:
         self.ingr = ingredientes
         self.prop = porcentagens
     
-    def le_receitas(self, receita):
+    def le_receitas(self):
         with open("receitas.json", "r") as fh:
             json_str = fh.read()
-            json_value_1 = json.loads(json_str)
-        receitas = json_value_1
+            json_value = json.loads(json_str)
+        return json_value
+    
+    def calcula_receitas(self, receita):
+        print("Receita para 4,5 Kg")
 
 class Ingredientes:
     def imprime_ingredientes():
         with open("ingredientes.json", "r") as fh:
             json_str = fh.read()
-            json_value_1 = json.loads(json_str)
-        ingr = json_value_1
+            json_value = json.loads(json_str)
+        ingredientes = json_value
         
-        for i in range(0, len(ingr)):
+        for i in range(0, len(ingredientes)):
             if i != 0:
                 print()
-            print(u"* {} ({})".format(
-                ingr[i]["nome"][0].capitalize(), 
-                ingr[i]["formula"][1]
+            print(u"{}) {} ({})".format(
+                ingredientes[i]["id"],
+                ingredientes[i]["nome"][0].capitalize(), 
+                ingredientes[i]["formula"][1]
             ))
             print("  - Para maiores informações, acessar:")
-            print(u"    - {}".format(ingr[i]["wiki"]["pt"]))
+            print(u"    - {}".format(ingredientes[i]["wiki"]["pt"]))
 
 Ingredientes.imprime_ingredientes()
+# Receita.le_receitas()
