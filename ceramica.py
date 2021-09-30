c7262e957ff78468e47dd1f447c8b2e4636b4a23
tomasfn87 <tomas.nalle@alumni.usp.br>
2021-09-30 11:13:30 -0300

    Alterada a função "Receita.__init__()": renomeadas as variáveis "self.ingr" > "self.i" e "self.prop" > "self.p". Adicionadas as funções a "class Receita": "nova_receita()" e "seleciona_ingredientes()". Alterada a função "Ingredientes.imprime_ingredientes()" > "Ingredientes.imprime_ingredientes(formato)": o parâmetro formato, que por padrão vem com valor "curto", pode receber o valor "longo" para imprimir informações mais detalhadas sobre o ingrediente.

--- ceramica.py
+++ ceramica.py
@@ -2,35 +2,53 @@ import json
 
 class Receita:
     def __init__(self, ingredientes, porcentagens):
-        self.ingr = ingredientes
-        self.prop = porcentagens
+        self.i = ingredientes
+        self.p = porcentagens
     
     def le_receitas(self):
         with open("receitas.json", "r") as fh:
             json_str = fh.read()
             json_value = json.loads(json_str)
         return json_value
+
+    def nova_receita(self):
+        with open("receitas.json", "w") as fh:
+            json_str = fh.read()
+            json_value = json.loads(json_str)
+        receitas = json_value
+        # Receita.seleciona_ingredientes()
+        i = [1, 2]
+        p = [20, 80]
+        assert p.extend() >= 100 \
+            and p.extend() <= 115
+        receita = Receita()
     
-    def calcula_receitas(self, receita):
+    def seleciona_ingredientes():
+        Ingredientes.imprime_ingredientes()
+
+    def calcula_receita(self, receita):
         print("Receita para 4,5 Kg")
 
 class Ingredientes:
-    def imprime_ingredientes():
+    def imprime_ingredientes(longo="curto"):
         with open("ingredientes.json", "r") as fh:
             json_str = fh.read()
             json_value = json.loads(json_str)
         ingredientes = json_value
         
         for i in range(0, len(ingredientes)):
-            if i != 0:
+            if i != 0 and longo == "longo":
                 print()
             print(u"{}) {} ({})".format(
                 ingredientes[i]["id"],
                 ingredientes[i]["nome"][0].capitalize(), 
                 ingredientes[i]["formula"][1]
             ))
-            print("  - Para maiores informações, acessar:")
-            print(u"    - {}".format(ingredientes[i]["wiki"]["pt"]))
+            if longo == "longo":
+                print("  - Para maiores informações, acessar:")
+                print(u"    - {}".format(ingredientes[i]["wiki"]["pt"]))
 
 Ingredientes.imprime_ingredientes()
+print()
+Ingredientes.imprime_ingredientes("longo")
 # Receita.le_receitas()
